f4d6f9b0b22e81d2ed0bb43b7fa23b8701735d10
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-02-10 20:46:27 +0100

    code cleanup and naming

--- popup/index.js
+++ popup/index.js
@@ -1,19 +1,15 @@
-import { copy_to_clipboard } from "../lib/clipboard.js";
-import { urls_to_markdown_list } from "../lib/formatter.js";
-
-document.getElementById("format-result").innerHTML = urls_to_markdown_list(
-  ["https://deno.land", "https://developer.chrome.com/docs/extensions"],
-);
+import { copyToClipboard } from "../lib/clipboard.js";
+import { urlsToMarkdownList as urlsToMarkdownList } from "../lib/formatter.js";
 
 document.getElementById("btn-dump-to-clipboard").addEventListener(
   "click",
-  (event) => {
+  () => {
     chrome.tabs.query({}, (tabs) => {
       const urls = tabs.map((it) => {
         return { title: it.title, url: it.url };
       });
-      const markdown_urls_list = urls_to_markdown_list(urls);
-      copy_to_clipboard(markdown_urls_list);
+      const markdownUrlsList = urlsToMarkdownList(urls);
+      copyToClipboard(markdownUrlsList);
     });
   },
 );
