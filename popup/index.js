49fcdc50d624b3c093eccaf6803955b827f624e1
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-02-10 20:09:51 +0100

    format tabs url to markdown

--- popup/index.js
+++ popup/index.js
@@ -4,7 +4,14 @@ document.getElementById("format-result").innerHTML = urls_to_markdown_list(
   ["https://deno.land", "https://developer.chrome.com/docs/extensions"],
 );
 
-document.getElementById("btn-dump-to-clipboard").addEventListener("click", (event)=>{
-  console.log("Hello click!")
-  //chrome.tabs.query(queryInfo: object, callback: function)
-})
+document.getElementById("btn-dump-to-clipboard").addEventListener(
+  "click",
+  (event) => {
+    console.log("Hello click!");
+    chrome.tabs.query({}, (tabs) => {
+      const urls = tabs.map((it) => it.url);
+      const markdown_urls_list = urls_to_markdown_list(urls);
+      console.log(markdown_urls_list);
+    });
+  },
+);
