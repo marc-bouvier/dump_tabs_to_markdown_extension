a7051fa7472580d7af1d3720a45181da4566df20
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-02-10 20:04:00 +0100

    button to dump tabs to clipboard

--- popup/index.js
+++ popup/index.js
@@ -3,3 +3,8 @@ import { urls_to_markdown_list } from "../lib/formatter.js";
 document.getElementById("format-result").innerHTML = urls_to_markdown_list(
   ["https://deno.land", "https://developer.chrome.com/docs/extensions"],
 );
+
+document.getElementById("btn-dump-to-clipboard").addEventListener("click", (event)=>{
+  console.log("Hello click!")
+  //chrome.tabs.query(queryInfo: object, callback: function)
+})
