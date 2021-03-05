25368189c176851bc4ee26b71e0b62502d90c203
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-03-05 20:34:55 +0100

    check ci detects test fail with make

--- tests/markdown_format_test.js
+++ tests/markdown_format_test.js
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts";
 import { urlsToMarkdownList } from "../shared/lib/formatter.js";
 
 Deno.test("assert framework is working", () => {
-  assertEquals(3, 3);
+  assertEquals(3, 5);
 });
 
 Deno.test("empty URL list formats to empty string", () => {
