344615da178e00cbb4eb223ce7662585c216c91f
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-03-05 20:35:58 +0100

    revert to passing test

--- tests/markdown_format_test.js
+++ tests/markdown_format_test.js
@@ -1,8 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts";
 import { urlsToMarkdownList } from "../shared/lib/formatter.js";
 
-Deno.test("assert framework is working", () => {
-  assertEquals(3, 5);
+Deno.test("assert test framework is working", () => {
+  assertEquals(3,3);
 });
 
 Deno.test("empty URL list formats to empty string", () => {
